259d390dfdec52b4f3b6722892aaaccfdcdd9af1
Julian <jjvll90@gmail.com>
2024-06-04 20:58:27 -0500

    Add sidebar styles to index.css

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,17 @@
 export default function SideBar() {
     return (
-        <div>
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
             <h2>The Brutal Martain Landscape</h2>
             <div>
                 <p>Description</p>
                 <p>Test Test Test Test Test Test Test Test </p>
             </div>
-
+            <button>
+            <i className="fa-solid fa-arrow-right"></i>
+            </button>
+            </div>
         </div>
     )
 }
